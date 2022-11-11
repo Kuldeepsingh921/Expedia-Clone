e8c186bce93b66d733784e487b170505b4fd718f
Kshitija Dokarmare <kshitijadokarmare786@gmail.com>
2022-11-11 22:08:31 +0530

    Day-4 Task Completed

---
+++ footer/footer.js
@@ -0,0 +1,50 @@
+
+function footer () {
+    return ` <div id="f123">
+    <div>
+     <img id="footerimg" src="https://a.travel-assets.com/globalcontrols-service/content/f285fb631b0a976202ef57611c7050e9ef5ca51a/images/EG_Wordmark_blue_RGB.svg" alt="">
+    </div>
+      <div>
+          <h2>Company</h2>
+          <a href="">About</a><br><br>
+          <a href="">Jobs</a><br><br>
+          <a href="">List your property</a><br><br>
+          <a href="">Partnerships</a><br><br>
+      </div>
+      <div>
+          <h2>Explore</h2>
+          <a href="">India travel guide</a><br><br>
+          <a href="">Hotels in India</a><br><br>
+          <a href="">Holiday rentals in India</a><br><br>
+          <a href="">Holiday packages in India</a><br><br>
+          <a href="">Domestic flights</a><br><br>
+          <a href="">Car hire in India</a><br><br>
+          <a href="">All accommodation types</a><br><br>
+          <a href="">Travel Blog</a><br><br>
+      </div>
+      <div>
+          <h2>Policies</h2>
+          <a href="">Privecy Statement</a><br><br>
+          <a href="">Terms of Use</a><br><br>
+          <a href="">Verbo terms and conditions</a><br><br>
+      </div>
+      <div>
+          <h2>Help</h2>
+          <a href="">Support</a><br><br>
+          <a href="">Change or cancel your booking</a><br><br>
+          <a href="">Refund process and timelines</a><br><br>
+          <a href="">Booka flight using an airline credit</a><br><br>
+          <a href="">International travel documents</a><br><br>
+      </div>
+     </div>
+ 
+ 
+     <div id="lastf">
+         <img src="https://images.trvl-media.com/media/content/expind/images/sg/secure.png" alt="">
+      <p>© 2022 Expedia, Inc., an Expedia Group company. All rights reserved. Expedia and the Airplane Logo are trademarks or registered trademarks of Expedia, Inc.</p>
+     </div>`
+}
+
+
+
+export {footer};
