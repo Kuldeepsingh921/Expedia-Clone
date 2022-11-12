41ac088a7908b63518d0458c0d8ec012a4dc9113
adityagithubraj <adityaraj9843@gmail.com>
2022-11-12 07:49:29 +0530

    support page day-5

--- support.js
+++ support.js
@@ -5,3 +5,16 @@ document.getElementById("button").addEventListener("click",function(){
 document.querySelector(".close").addEventListener("click",function(){
     document.querySelector(".popup").style.display="none"
 })
+
+document.getElementById("send").addEventListener("click",function(){
+    
+    
+    var heading = document.createElement("p");
+    heading.id = "crestTitle";
+    
+    
+    element = document.getElementById("mobnum");
+    heading.innerText="  ✓  Thank you! A text with the download link has been sent to your   6295750823   mobile device";
+    element.appendChild(heading);
+
+})
